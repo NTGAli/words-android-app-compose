eca2939022e4f1ea9eac829ef3fc389730abc8f1
NTGAli <alintg@outlook.com>
2024-05-17 09:49:50 +0330

    Fix multiple backup

--- app/src/main/java/com/ntg/vocabs/util/backup/FirebaseBackupWorker.kt
+++ app/src/main/java/com/ntg/vocabs/util/backup/FirebaseBackupWorker.kt
@@ -8,9 +8,7 @@ import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.ntg.vocabs.db.AppDB
-import com.ntg.vocabs.db.dao.WordDao
 import com.ntg.vocabs.model.db.TimeSpent
-import com.ntg.vocabs.model.db.Word
 import com.ntg.vocabs.model.db.toMap
 import com.ntg.vocabs.util.Constant
 import com.ntg.vocabs.util.Constant.BackTypes.BACKUP_LISTS
@@ -19,18 +17,17 @@ import com.ntg.vocabs.util.Constant.BackTypes.BACKUP_TIMES
 import com.ntg.vocabs.util.Constant.BackTypes.BACKUP_WORDS
 import com.ntg.vocabs.util.orDefault
 import com.ntg.vocabs.util.orFalse
-import com.ntg.vocabs.util.orTrue
 import com.ntg.vocabs.util.timber
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.time.LocalDate
-import kotlin.reflect.KClass
 
 class FirebaseBackupWorker(
     private val appContext: Context,
     private val workerParams: WorkerParameters,
 ) : CoroutineWorker(appContext, workerParams) {
+
 
 
     override suspend fun doWork(): Result {
@@ -40,6 +37,7 @@ class FirebaseBackupWorker(
 
         val email = inputData.getString("email")
 
+
         if (email.orEmpty().isNotEmpty()){
             when (val type = inputData.getString("type")) {
 
@@ -48,12 +46,9 @@ class FirebaseBackupWorker(
 
                         it.forEach { word ->
                             if (word.isDeleted.orFalse()) {
-                                timber("addOnFailureListener ::: www :::: $word")
 
                                 if (word.fid != null){
                                     deleteOnFirestore(word.fid.orEmpty(), type) {
-                                        timber("addOnFailureListener ::: vvv :::: $it")
-
                                         if (it) {
                                             CoroutineScope(Dispatchers.IO).launch {
                                                 appDB.wordDao().delete(word)
@@ -92,16 +87,10 @@ class FirebaseBackupWorker(
 
                 BACKUP_LISTS -> {
                     appDB.vocabListDao().getUnSyncedLists().let {
-                        timber("getUnSyncedWords :::: $it")
-
                         it.forEach { list ->
                             if (list.isDeleted.orFalse()) {
-                                timber("addOnFailureListener ::: www :::: $list")
-
                                 if (list.fid != null){
                                     deleteOnFirestore(list.fid, type) {
-                                        timber("addOnFailureListener ::: vvv :::: $it")
-
                                         if (it) {
                                             CoroutineScope(Dispatchers.IO).launch {
                                                 appDB.vocabListDao().delete(list)
@@ -217,14 +206,15 @@ class FirebaseBackupWorker(
     ) {
         val firestore = Firebase.firestore
 
-        firestore.collection(type).add(data)
+        firestore
+            .collection(type).add(data)
             .addOnSuccessListener {
                 CoroutineScope(Dispatchers.IO).launch {
                     isSuccess.invoke(it)
                 }
 
             }
-            .addOnFailureListener { e -> timber("BBBCCCBB ::: Error writing document :::: $e") }
+            .addOnFailureListener { e -> timber("backupOnFirestore ::: Error writing document :::: $e") }
 
     }
 
@@ -247,16 +237,12 @@ class FirebaseBackupWorker(
 
 
     private fun deleteOnFirestore(fid: String, type: String, isSuccess: (Boolean) -> Unit) {
-        timber("addOnFailureListener ::: ............")
-
         val firestore = Firebase.firestore
         firestore.collection(type).document(fid)
             .delete()
             .addOnSuccessListener {
-                timber("addOnFailureListener ::: ---------")
                 isSuccess.invoke(true) }
             .addOnFailureListener { e ->
-                timber("addOnFailureListener ::: ${e.message}")
                 isSuccess.invoke(false) }
     }
 }
